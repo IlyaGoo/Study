0ff66d60d7a49f1201b6162f1f0d88a92a173a6d
Ilya <ilyagooooo@yandex.ru>
2020-10-11 17:09:33 +0500

    https://youtu.be/0QHj05rlE_Q

--- src/main/scala/example/First/First.scala
+++ src/main/scala/example/First/First.scala
@@ -1,4 +1,7 @@
 package example.First
+import scala.math._
+import scala.util.Random
+import scala.BigInt._
 
 object First {
   println("First topic")
@@ -21,4 +24,37 @@ object First {
   //a НазваниеМетода b - непривычная запись, нужно привыкнуть
   //Оператора ++ нет
   println("Ilya".toSeq.sorted) //Если у метода нет параметров,Ю можно писать без скобок
+  println(sqrt(4)); println(scala.math.sqrt(4)) //Второй случай, если не импортировали библиотеку
+
+  probablePrime(100, scala.util.Random) //Тут BigInt - объект компаньон класса BigInt, его методы играют роль статических методов
+
+  val str = "Qwerty"
+  println(str(2)) //равносильно str.apply(2)
+  println(Array.apply(1 to 2))
+
+  //Упражение 2
+  var res : Double = 3
+  res = sqrt(res)
+  res = res * res
+  println(res)
+
+  //Упражнение 4
+  println("crazy" * 3)
+
+  //Упражение 5
+  println(10 max 12)
+
+  //val resInt : BigInt = pow(2, 1024) //Разобраться как это сделать без циклов
+  //println(resInt)
+
+  //Упражнение 7
+  println(probablePrime(100, Random))
+
+  //Упражение 8
+  val randomInt : Int = Random.nextInt()
+  println(randomInt.toString(36))
+
+  //Упражение 9
+  println(str(0))
+  println(str(str.length - 1))
 }
