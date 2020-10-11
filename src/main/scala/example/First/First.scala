a42dbecd3b924740c06c141ecd9a375d53917c98
Ilya <ilyagooooo@yandex.ru>
2020-10-11 14:29:16 +0500

    https://youtu.be/0QHj05rlE_Q

---
+++ src/main/scala/example/First/First.scala
@@ -0,0 +1,8 @@
+package example.First
+
+object First {
+  println("First topic")
+
+  val rea2: Int = 123 + 2 * 7
+  println(rea2)
+}
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,19 +1,7 @@
 package example
 
-object Main extends App {
-  abstract class A {
-    val message: String
-  }
-  class B extends A {
-    val message = "I'm an instance of class B"
-  }
-  trait C extends A {
-    def loudMessage: String = message.toUpperCase()
-    val tuple = (213, "asd")
-  }
-  class D extends B with C
+import example.First.First
 
-  val d = new D
-  println(d.message)  // I'm an instance of class B
-  println(d.loudMessage)
+object Main extends App {
+  First
 }
