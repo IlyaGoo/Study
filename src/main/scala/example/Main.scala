7aa78fcbf748e95af711944f17b1649e742a7f1b
Ilya <ilyagooooo@yandex.ru>
2020-10-11 18:32:21 +0500

    https://youtu.be/0QHj05rlE_Q

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,9 @@
 package example
 
-import example.First.First
+import example.First._
+import example.Second._
 
 object Main extends App {
-  First
+  //First
+  Second
 }
---
+++ src/main/scala/example/Second/Second.scala
@@ -0,0 +1,6 @@
+package example.Second
+
+object Second {
+  val n : Int = if (true) 1 else -1
+  println(n)
+}
