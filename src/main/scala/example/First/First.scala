a712a96bd672ca3f78abcc936a2419e801330119
Ilya <ilyagooooo@yandex.ru>
2020-10-11 15:30:54 +0500

    https://youtu.be/0QHj05rlE_Q

--- src/main/scala/example/First/First.scala
+++ src/main/scala/example/First/First.scala
@@ -2,7 +2,13 @@ package example.First
 
 object First {
   println("First topic")
+  //val == константа, var - нет
+  val answer: Int = 123 + 2 * 7
+  println(answer)
+  var counter = 0
+  counter = 1
 
-  val rea2: Int = 123 + 2 * 7
-  println(rea2)
+  // ; используется, если несколько инструкций в одной строчке
+
+  val x, y = 2 //Можно объявлять сразу несколько переменных
 }
