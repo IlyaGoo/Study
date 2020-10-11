cdfe63d22c60a3c5bc47c21e580aa2e42e2b6576
Ilya <ilyagooooo@yandex.ru>
2020-10-11 19:00:51 +0500

    https://youtu.be/0QHj05rlE_Q

--- src/main/scala/example/Second/Second.scala
+++ src/main/scala/example/Second/Second.scala
@@ -3,4 +3,7 @@ package example.Second
 object Second {
   val n : Int = if (true) 1 else -1
   println(n)
+
+  val index : Int = { val i1 = 1; val i2 = 2; i1 + i2}//Результатам скобок является значение последнего выражения
+  println(index)
 }
