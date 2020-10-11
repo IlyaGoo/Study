6dbdf4474657b69eb5827907ceb063f787ce03cf
Ilya <ilyagooooo@yandex.ru>
2020-10-11 20:08:57 +0500

    https://youtu.be/0QHj05rlE_Q

--- src/main/scala/example/Second/Second.scala
+++ src/main/scala/example/Second/Second.scala
@@ -6,4 +6,10 @@ object Second {
 
   val index : Int = { val i1 = 1; val i2 = 2; i1 + i2}//Результатам скобок является значение последнего выражения
   println(index)
+
+  val index2 : Unit = {val t = -1} //операция присвивание это Unit, имеющий единственное значние (пустое)
+
+  for(i <- 1 to 3)
+    print(f"n${i} ")
+  println()
 }
